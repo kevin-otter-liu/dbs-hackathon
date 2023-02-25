44ff7a6c9733a8327e82b5f51f0e781cca950e82
darellelogram <darrell.lai@gmail.com>
2023-02-25 13:07:19 +0800

    Started edit and create forms

---
+++ frontend/dbs-frontend/src/components/EditForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+
+const EditForm = () => {
+  return (
+    <div className="container">
+        <div className="col edit-first-col">
+            Insurance Claim {id}: 
+        </div>
+        <p>Insurance Claim {id}</p>
+        <input></input>
+        <p>Date</p>
+        <p>Amount</p>
+        <p>is follow up claim?</p>
+        <p></p>
+        
+
+    </div>
+
+  )
+}
+
+export default EditForm

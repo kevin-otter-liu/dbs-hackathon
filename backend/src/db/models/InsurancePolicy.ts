d745490f14484b8b5f0a092f1a5a8c562ae6661b
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2023-02-25 13:15:03 +0800

    Add insurance policy model

---
+++ backend/src/db/models/InsurancePolicy.ts
@@ -0,0 +1,61 @@
+import {
+    Model,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    ForeignKey,
+  } from 'sequelize';
+  
+  import getDbConnection from '../db-config';
+import Employee from './User';
+  
+  // import Token from './Token';
+  
+  // defining Models: attributes at creation and attributes output from DB
+  class InsurancePolicy extends Model<
+    InferAttributes<InsurancePolicy>,
+    InferCreationAttributes<InsurancePolicy>
+  > {
+    declare insuranceid: number;
+    declare employeeid: ForeignKey<Employee['employeeid']>;
+    declare insurancetype: string;
+    declare policystartdate: string;
+    declare policyterm: string;
+    declare policyenddate: string;
+    declare claimlimit: number;
+    declare remainingclaimlimit: number;
+
+  }
+  
+  InsurancePolicy.init(
+    {
+      insuranceid: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      insurancetype: {
+        type: DataTypes.STRING,
+      },
+      policystartdate: {
+        type: DataTypes.STRING,
+      },
+      policyterm: {
+        type: DataTypes.STRING,
+      },
+      policyenddate: {
+        type: DataTypes.STRING,
+      },
+      claimlimit: {
+        type: DataTypes.FLOAT,
+      },
+      remainingclaimlimit: {
+        type: DataTypes.FLOAT,
+      },
+    },
+    { sequelize: getDbConnection(), modelName: 'insurancepolicy' }
+  );
+  
+  export default InsurancePolicy;
+  

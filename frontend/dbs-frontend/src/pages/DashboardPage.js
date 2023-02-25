586b48d5dda240d8c95ec3666945842fb16c6c64
benlkm <benvmon2@Hotmail.com>
2023-02-25 14:38:01 +0800

    Dashboard

---
+++ frontend/dbs-frontend/src/pages/DashboardPage.js
@@ -0,0 +1,16 @@
+import React , { useState, useEffect } from "react";
+import Dashboard from "../components/Dashboard";
+
+
+const DashboardPage = () =>
+{
+    return (
+        <div>
+        <Dashboard />
+
+        </div>
+    )
+
+}
+
+export default DashboardPage

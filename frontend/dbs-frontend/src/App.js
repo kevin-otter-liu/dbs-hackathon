2d0f9d34cd51fcfacb4812cf77a133dff2ff1c63
darellelogram <darrell.lai@gmail.com>
2023-02-25 16:08:11 +0800

    Adding Edite Route

--- frontend/dbs-frontend/src/App.js
+++ frontend/dbs-frontend/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
+// import CreatePage from './pages/CreateFormPage';
+import EditPage from './pages/EditPage';
+import React from "react";
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path='/' element={<LoginPage />} />
+          <Route path='/Edit' element={<EditPage />} />
         </Routes>
       </div>
     </Router>

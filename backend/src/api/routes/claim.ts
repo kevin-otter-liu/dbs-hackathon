6e6fd417cdbdb791b1c223149e2e683adb9aac43
fanminj <fanmin98@gmail.com>
2023-02-25 15:39:16 +0800

    Add endpoint to get all insurance claims

--- backend/src/api/routes/claim.ts
+++ backend/src/api/routes/claim.ts
@@ -1,22 +1,37 @@
 import { Router } from 'express';
 import EmployeeModel from "../../db/models/User";
-import {HttpError} from "../../libs/http-error";
+import { HttpError } from "../../libs/http-error";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { CustomRequest } from '../middleware/check-auth';
+import InsurancePolicy from '../../db/models/InsurancePolicy';
+import InsuranceClaim from '../../db/models/InsuranceClaim';
+import { Op } from 'sequelize';
 
 const ClaimRouter = Router();
 
 ClaimRouter.get('/', async (req, res, next) => {
-    // Returns the list of claim records the employee has
-    const { accessToken } = req.body;
+    const { employeeid } = (req as CustomRequest).employee;
+    const policies = await InsurancePolicy.findAll({
+        where: { employeeid },
+    });
 
-    // Check accessToken validity
-    // If accessToken is not valid, return 400 INVALID USER
-    // else, return 200 OK
-    // TODO: Replace dummy 200 OK
-    res.status(200).json({
-        policies: []
-    })
+    if (policies.length === 0) {
+        res.status(200).json({
+            claims: []
+        });
+    }
+
+    const insuranceids = policies.map(p => p.insuranceid);
+
+    const claims = await InsuranceClaim.findAll({
+        where: {
+            [Op.or]: insuranceids.map(insuranceid => ({ insuranceid }))
+        }
+    });
+
+    res.status(200).json({ claims });
+    return next();
 });
 
 ClaimRouter.post('/', async (req, res, next) => {

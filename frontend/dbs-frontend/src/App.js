321d2bceb6c1d2656bbac9e5d66c66cf1cfb8224
Ben <36397189+benlkm@users.noreply.github.com>
2023-02-25 16:38:30 +0800

    Revert "Frontend/setup"

--- frontend/dbs-frontend/src/App.js
+++ frontend/dbs-frontend/src/App.js
@@ -1,19 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import CreateFromPage from "./pages/CreateFromPage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/create" element={<CreateFromPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
